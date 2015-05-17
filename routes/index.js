e7be758eedd3a736215dd92f65a9c2ac75ef8eaa
walkingtospace <walkingtosppace@gmail.com>
2015-05-17 20:05:27 +0000

    finish implementatio of roundrobin basics

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var hosts = require('../configs/hosts.json');
 var hostsize = hosts.hosts.length;
 var hostcount = 0;
+var middleware = require('../controllers/middleware');
 
 exports.connect = function(app) {
 	app.get('/', index); 
@@ -20,11 +21,18 @@ function roundrobin(req, res, next) {
 	}	
 
 	//redirect requests to host
-	
+	console.log("[roundrobin] IP: " + hosts.hosts[hostcount].IP + " port:" + hosts.hosts[hostcount].port); 
+	middleware.redirector(hosts.hosts[hostcount].IP, hosts.hosts[hostcount].port, res, send);
 }
 
 function resourcebase(res, res, next) {
 	//get hosts status
 	//chooose idle host
 	//redirect requests to host
+}
+
+function send(res, resFromHost) {
+	console.log("send back to Host Agent");
+	
+	res.send("finish");
 }
