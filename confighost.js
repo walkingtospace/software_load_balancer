64c03ed4480fa6e08c3c64a10ec7a76b3d580726
Thalley <mrthalley@gmail>
2015-05-29 12:25:26 -0700

    Updated confighost.js to include ip

--- confighost.js
+++ confighost.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var exec = require('child_process').execSync;
+var dns = require('dns');
 var path;
 
 // Finds and adds all servers to the hosts.json file in the configs folder
@@ -15,9 +16,9 @@ if(process.argv.length < 3){
 			path = "/proj/Reactor/exp/" + experiments[i] + "/tbdata/tbreport.log";
 			break;
 		}
-}
-else
-	path = process.argv.slice(2).toString();
+	}
+	else
+		path = process.argv.slice(2).toString();
 
 
 // Read the file as lowercase
@@ -25,22 +26,37 @@ var file = fs.readFileSync(path).toString().toLowerCase();
 //Split the file into strings separated by space so we can finde hostnames
 var strings = file.split(/[ \n]+/); 
 // Find all hostnames
-var hostnames = strings.filter(function(s){return s.indexOf("reactor.") >= 0;}); 
+var nodenames = strings.filter(function(s){return s.indexOf("reactor.") >= 0;}); 
 // Find all hostnames that are not a LB or a traffic generator
 // Assumes a naming convention where every LB is named LBx (x is an int) and the traffic generator is named Generator
-var servers = hostnames.filter(function(s){var nodename = (s.split(/[.]/))[0]; return nodename.indexOf("lb") < 0 && nodename.indexOf("generator") < 0;});
+var hostnames = nodenames.filter(function(s){
+									var nodename = (s.split(/[.]/))[0]; 
+									return nodename.indexOf("lb") < 0 && nodename.indexOf("generator") < 0;
+								});
 
 //Create JSON
-var hostjson = '{ "hosts": [';
-servers.forEach(function(server){hostjson += '{ "IP":"' + server + '","port":' + "3000" + '},'});
-hostjson = hostjson.substring(0, hostjson.length - 1); //Remove ekstra comma
-hostjson += '] }';
-var prettyJSON = JSON.stringify(JSON.parse(hostjson), null, 2);
-
-//Write to file
-fs.writeFile("./configs/hosts.json", prettyJSON, function(err) {
-    if(err) {
-        return console.log(err);
-    }
-    console.log("The file was saved!");
+var hostjson = '{ "hosts": [ ';
+var addresses = "ip";
+//Insert ip, port and hostname for each hostname
+hostnames.forEach(function(hostname){
+	dns.lookup(hostname, function onLookup(err, addresses, family) {
+		hostjson += '{ "IP":"' + addresses + '","port":' + "3000" + ',"hostname":"' + hostname + '"},';
+	});
 });
+
+setTimeout(printHostfile, 10);
+function printHostfile(){
+	hostjson = hostjson.substring(0, hostjson.length - 1); //Remove ekstra comma
+	hostjson += '] }';
+	var prettyJSON = JSON.stringify(JSON.parse(hostjson), null, 2);
+
+	//Write to file
+	fs.writeFile("./configs/hosts.json", prettyJSON, function(err) {
+		if(err) {
+			return console.log(err);
+		}
+		console.log("The file was saved!");
+	});
+
+}
+
