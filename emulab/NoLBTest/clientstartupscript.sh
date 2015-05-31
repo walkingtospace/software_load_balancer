fca4feaedabdce95384044ee79557f89b728c17f
Thalley <mrthalley@gmail>
2015-05-30 20:41:08 -0700

    Updated the clientstartupscript for NoLBTest

--- emulab/NoLBTest/clientstartupscript.sh
+++ emulab/NoLBTest/clientstartupscript.sh
@@ -3,4 +3,4 @@ git pull
 #Stress can be induced on server by commention out the following line
 #node stress.js 50 10000 0
 node sysinfo.js &
-node --expose-gc webserver.js &
+node --expose-gc --max-old-space-size=5000 webserver.js &
