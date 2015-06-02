00cebda8f62c86ede57fa05edb02e24075bff573
Thalley <mrthalley@gmail>
2015-06-01 23:17:10 -0700

    LB now redirects by resourcebase by default and falls back to roundrobin

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ var masterCheckProcess = null;
 var queue = []; //host information
 
 exports.connect = function(app) {
-	app.get('/', index); 
+	app.get('/*', resourcebase); //Redirect all requests to resourcebase
 	app.get('/route/roundrobin*', roundrobin); //basic
 	app.get('/route/resourcebase/:type*', resourcebase); //CPU,MEMORY-based scheduling
 
@@ -132,28 +132,35 @@ function roundrobin(req, res, next) {
 	}	
 
 	//redirect requests to host
-	var reqURL = req.url.substr(17); // remove /route/roundrobin from url
+	var reqURL = req.url; // remove /route/roundrobin from url
  	console.log("[roundrobin] Redirect traffic to : " + hosts.hosts[hostcount].IP + " port:" + hosts.hosts[hostcount].port); 
 
 	middleware.redirector(hosts.hosts[hostcount].IP, hosts.hosts[hostcount].port, reqURL, res, send);
 }
 
 function resourcebase(req, res, next) {
+	if(req.url === "/compute")
+		req.params.type = constant.SERVER.CPU;
+	else if(req.url === "/memory")
+		req.params.type = constant.SERVER.MEM;
 	var nextHost = getPriority(req.params.type);	
-
+	console.log(queue);
 	if(nextHost === undefined) {
 		console.log("[CPU/MEM] Redirection failed");	
-	
-		res.status(200).send("Redirection failed");
+		console.log("[CPU/MEM] Falling back to roundrobin");
+		roundrobin(req, res, next);
+		// res.status(200).send("Redirection failed");
 	} else if(nextHost.IP !== undefined && nextHost.port !== undefined){
 		console.log("[CPU/MEM] Redirect traffic to : " + nextHost.IP + " port:" + nextHost.port); 
 		
-		var reqURL = req.url.substr(23); // remove /route/roundrobin from url
+		var reqURL = req.url; // remove /route/roundrobin from url
 		middleware.redirector(nextHost.IP, nextHost.port, reqURL, res, send, nextHost);
 	} else {
 		console.log("No available resources");
+		console.log("Falling back to roundrobin");
+		roundrobin(req, res, next);
 
-		res.status(200).send("No available resources");
+		// res.status(200).send("No available resources");
 	}
 }
 
@@ -163,7 +170,6 @@ function getPriority(type) {
 	
 	if(type === constant.SERVER.CPU) {
 		temp = {"cpu": constant.HOST.CPU_THRESHOLD};
-
 		for(var i in queue) {
 			if(temp.cpu > parseInt(queue[i].cpu)) {	
 				temp = queue[i];
@@ -180,6 +186,9 @@ function getPriority(type) {
 			}
 		}
 	}
+	//If no type was given
+	else
+		return undefined;
 
 	if(temp.cpu !== undefined && temp.cpu >= constant.HOST.CPU_THRESHOLD) {
 	
