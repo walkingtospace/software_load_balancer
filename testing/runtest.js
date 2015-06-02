121eb29e629600874d55d8e5f5d629fff3f7cbf2
Thalley <mrthalley@gmail>
2015-06-02 16:10:53 -0700

    Updated runtest.js with more information gathering

--- testing/runtest.js
+++ testing/runtest.js
@@ -3,18 +3,32 @@ var fs = require('fs');
 var hostagents = require('../configs/hostagent.json');
 var hostagent = hostagents.hostagent[0];
 var numConn = 100; // Total amount of requests to send
-var rate = 10; // requests per second
+var rate = 50; // requests per second
 var timeout = 10; //in seconds
 // var httperf = "httperf --server" + hostagent.hostagent.IP + " --port " + hostagent.hostagent.port + " --num-conn " + numConn + " --rate " + rate + " --wlog Y,wlog.log";
 var grep = " | grep 'Total'";
-var result = "";
+var result = "Number of requsts,Time,Min,Max,Avg,Median,Std. Dev.\n";
 for (i = 1; i <= 10; i++){
-	numConn = i * 10;
-	// var httperf = "httperf --server thalley.com --num-conn " + numConn + " --rate 50";
-	var httperf = "httperf --server " + hostagent.IP + " --port " + hostagent.port + " --num-conn " + numConn + " --rate " + rate + " --wlog Y,wlog.log";
+	numConn = i * 50;
+	var httperf = "httperf --server thalley.com --num-conn " + numConn + " --rate 50";
+	// var httperf = "httperf --server " + hostagent.IP + " --port " + hostagent.port + " --num-conn " + numConn + " --rate " + rate + " --wlog Y,wlog.log";
 	console.log(httperf);
-	var time = (exec(httperf + grep).toString().split(/[ ]/))[8];
-	result += numConn + "," + time + "\n";
+	var httperfRes = exec(httperf).toString();
+	// console.log(httperfRes);
+	var time = httperfRes.split(/[\n]/)[3].split(/[ ]/)[8];
+	var conTime = httperfRes.split(/[\n]/)[6].split(/[ ]/);
+	var min = conTime[4];
+	var avg = conTime[6];
+	var max = conTime[8];
+	var median = conTime[10];
+	var stddev = conTime[12];
+	console.log("Time:\t\t " + time);
+	console.log("Min:\t\t " + min);
+	console.log("Max:\t\t " + max);
+	console.log("Avg:\t\t " + avg);
+	console.log("Median:\t\t " + median);
+	console.log("Std. Dev.:\t " + stddev);
+	result += numConn + "," + time + "," + min + "," + max + "," + avg + "," + median + "," + stddev + "\n";
 }
 
 
