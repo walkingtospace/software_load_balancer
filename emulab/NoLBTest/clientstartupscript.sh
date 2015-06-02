257ed172c1dcc818e3bb1cfdc3bd084d0d9f1085
Thalley <mrthalley@gmail>
2015-06-02 00:30:02 -0700

    Updated constants for tests and added memory stress to clients

--- emulab/NoLBTest/clientstartupscript.sh
+++ emulab/NoLBTest/clientstartupscript.sh
@@ -1,6 +1,6 @@
 cd /proj/Reactor/software_load_balancer/client
 git pull
 #Stress can be induced on server by commention out the following line
-#node stress.js 50 10000 0
+node stress.js 0 10000 &
 node sysinfo.js &
 node --expose-gc --max-old-space-size=15000 webserver.js &
--- emulab/SingleLBTest/clientstartupscript.sh
+++ emulab/SingleLBTest/clientstartupscript.sh
@@ -1,4 +1,6 @@
-cd /proj/Reactor/software_load_balancer
+cd /proj/Reactor/software_load_balancer/client
 git pull
-node client/sysinfo.js &
-node client/webserver.js &
+#Stress can be induced on server by commention out the following line
+node stress.js 0 10000 &
+node sysinfo.js &
+node --expose-gc --max-old-space-size=15000 webserver.js &
