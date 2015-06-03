19c5c63da24093b97015fb617f91e583ad17cae8
Thalley <mrthalley@gmail>
2015-06-03 13:27:33 -0700

    Updated runtest to run several time and do statistics

--- testing/runtest.js
+++ testing/runtest.js
@@ -1,41 +1,43 @@
 var exec = require('child_process').execSync;
+var math = require('mathjs');
 var fs = require('fs');
 var hostagents = require('../configs/hostagent.json');
 var hostagent = hostagents.hostagent[0];
 var numConn = 100; // Total amount of requests to send
 var increase = 50; // requests per second
 var timeout = 10; //in seconds
-// var httperf = "httperf --server" + hostagent.hostagent.IP + " --port " + hostagent.hostagent.port + " --num-conn " + numConn + " --rate " + rate + " --wlog Y,wlog.log";
-var grep = " | grep 'Total'";
-// var result = "Number of requsts,Time,Min,Max,Avg,Median,Std. Dev.\n";
-var result = "Number of requsts,Time\n";
+var iterations = 1;
+if(process.argv.length > 2)
+	iterations = process.argv[2].toString();
+
+var resultStr = "Requsts,Time,Min,Max,Avg,Median,Std.Dev.\n";
 for (i = 1; i <= 10; i++){
 	numConn = i * increase;
-	// var httperf = "httperf --server thalley.com --num-conn " + numConn + " --rate 50";
-	//Rate is = numConn to send all requests in 1 second
-	var httperf = "httperf --server " + hostagent.IP + " --port " + hostagent.port + " --num-conn " + numConn + " --rate " + numConn + " --wlog Y,wlog.log";
+	var result = new Array(iterations);
+	// var httperf = "httperf --server " + hostagent.IP + " --port " + hostagent.port + " --num-conn " + numConn + " --rate " + numConn + " --wlog Y,wlog.log";
+	var httperf = "httperf --server thalley.com --hog ";
 	console.log(httperf);
-	var httperfRes = exec(httperf).toString();
-	// console.log(httperfRes);
-	var time = httperfRes.split(/[\n]/)[3].split(/[ ]/)[8];
-	var conTime = httperfRes.split(/[\n]/)[6].split(/[ ]/);
-	var min = conTime[4];
-	var avg = conTime[6];
-	var max = conTime[8];
-	var median = conTime[10];
-	var stddev = conTime[12];
+	for(j = 0; j < iterations; j++){
+		var httperfRes = exec(httperf).toString();
+		var time = httperfRes.split(/[\n]/)[3].split(/[ ]/)[8];
+		result[j] = parseFloat(time);
+	}
+	console.log(result);
+	var min = math.min(result);
+	var avg = math.mean(result);
+	var max = math.max(result);
+	var median = math.median(result); //In case of an even number of values, the average of the two middle values is returned. 
+	var stddev = math.std(result);
 	console.log("Time:\t\t " + time);
 	console.log("Min:\t\t " + min);
 	console.log("Max:\t\t " + max);
 	console.log("Avg:\t\t " + avg);
 	console.log("Median:\t\t " + median);
 	console.log("Std. Dev.:\t " + stddev);
-	result += numConn + "," + time + "\n";// + "," + min + "," + max + "," + avg + "," + median + "," + stddev + "\n";
+	resultStr += numConn + "," + time + "," + min + "," + max + "," + avg + "," + median + "," + stddev + "\n";
 }
 
-
-// var httperfTO = "httperf --server" + hostagent.hostagent.IP + " --port " + hostagent.hostagent.port + " --num-conn " + numConn + " --rate " + rate + " --wlog Y,wlog.log --timeout " + timeout;
-
+// Save to file: [expName]results
 var expname = (exec("hostname").toString().split(/[.]/))[1];
 fs.writeFile(expname + "results", result, function(err) {
 	if(err) {
@@ -43,5 +45,3 @@ fs.writeFile(expname + "results", result, function(err) {
 	}
 	console.log("The file was saved!");
 });
-
-// var timeTO = exec(httperfTO).toString();
