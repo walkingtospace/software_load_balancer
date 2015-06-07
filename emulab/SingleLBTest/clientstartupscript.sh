ca120e2b340a37d0951263d7baa8eecbc6eba5df
Thalley <mrthalley@gmail>
2015-06-06 22:46:41 -0700

    runtestTotal now measure the avg cpu of the end-hosts

--- emulab/SingleLBTest/clientstartupscript.sh
+++ emulab/SingleLBTest/clientstartupscript.sh
@@ -1,5 +1,4 @@
 cd /proj/Reactor/software_load_balancer/client
-#Stress can be induced on server by commention out the following line
-# node stress.js 0 10000 &
+node measureCPUjs &
 node sysinfo.js &
 node --expose-gc webserver.js &
--- testing/runtestTotal.js
+++ testing/runtestTotal.js
@@ -1,40 +1,61 @@
 var exec = require('child_process').execSync;
 var math = require('mathjs');
 var fs = require('fs');
+var request = require("request");
+var constant = require('../configs/constants.json');
+var hosts = require('../configs/hosts.json');
 var hostagents = require('../configs/hostagent.json');
 var hostagent = hostagents.hostagent[0];
 var numConn = 100; // Total amount of requests to send
 var increase = 20; // requests per second
 var timeout = 10; //in seconds
-var iterations = 1;
+var iterations = 2;
 if(process.argv.length > 2)
 	iterations = process.argv[2].toString();
 
-var resultStr = "Requsts,Time,Min,Max,Avg,Median,Std.Dev.\n";
-for (i = 1; i <= 10; i++){
+var resultStr = "Requsts,Time,Min,Max,Avg,Median,Std.Dev.,CPU\n";
+for (i = 1; i <= 1; i++){
 	numConn = i * increase;
 	var result = new Array(iterations);
+	var cpus = new Array(iterations);
 	var httperf = "httperf --server " + hostagent.IP + " --port " + hostagent.port + " --num-conn " + numConn + " --rate " + increase + " --wlog Y,wlog.log";
-	// var httperf = "httperf --server thalley.com --hog ";
+	// var httperf = "httperf --server localhost --uri / --port 3000 --num-conn 100 --rate 50";
 	console.log(httperf);
 	for(j = 0; j < iterations; j++){
+		// Start measuring CPU
+		for(k = 0; k < hosts.hosts.length; k++)
+			sendReq(hosts.hosts[k].IP, 'start', j, function(){}); 
+
+		// Send requests
 		var httperfRes = exec(httperf).toString();
+
+		// Get CPU measurements
+		for(k = 0; k < hosts.hosts.length; k++)
+			sendReq(hosts.hosts[k].IP, 'end', j, function(res, index){
+				cpus[index] = parseFloat(res);
+			});
+
+		// Get time
 		var time = httperfRes.split(/[\n]/)[3].split(/[ ]/)[8];
 		result[j] = parseFloat(time);
 	}
+
 	console.log(result);
+	console.log(cpus);
 	var min = math.min(result);
 	var avg = math.mean(result);
 	var max = math.max(result);
 	var median = math.median(result); //In case of an even number of values, the average of the two middle values is returned. 
 	var stddev = math.std(result);
+	var cpu = math.mean(cpus);
 	console.log("Time:\t\t " + time);
 	console.log("Min:\t\t " + min);
 	console.log("Max:\t\t " + max);
 	console.log("Avg:\t\t " + avg);
 	console.log("Median:\t\t " + median);
 	console.log("Std. Dev.:\t " + stddev);
-	resultStr += numConn + "," + time + "," + min + "," + max + "," + avg + "," + median + "," + stddev + "\n";
+	console.log("CPU:\t " + cpu);
+	resultStr += numConn + "," + time + "," + min + "," + max + "," + avg + "," + median + "," + stddev + cpu + "\n";
 }
 
 // Save to file: [expName]results
@@ -45,3 +66,13 @@ fs.writeFile(expname + "resultsTotal", resultStr, function(err) {
 	}
 	console.log("The file was saved!");
 });
+
+// Send request to end-hosts to start measuring CPU
+function sendReq(host, req, j, callback) {
+	var url = 'http://' + host + ':' + constant.MEASUREMENT.PORT + '/' + req;
+	request(url, function (error, response, res) {
+		if (!error && response.statusCode == 200) {
+			callback(res, j);
+		}
+	})
+}
