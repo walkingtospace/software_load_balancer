eb3dac19b398d40709b5a781f4593d5d3f5c89e9
Thalley <mrthalley@gmail>
2015-06-06 23:40:46 -0700

    Updated reset

--- emulab/reset.js
+++ emulab/reset.js
@@ -4,11 +4,11 @@ var hostagents = require('../configs/hostagent.json');
 var constant = require('../configs/constants.json');
 var lbs = require('../configs/slaves.json');
 var expname = (exec("hostname").toString().split(/[.]/))[1];
-hosts.hosts.forEach(host){
+hosts.hosts.forEach(function(host){
     var ssh = "ssh thalley@" + host.IP + " ";
     exec(ssh + "killall node");
     exec(ssh + "/proj/Reactor/software_load_balancer/emulab/" + expname + "/clientstartupscript.sh");
-}
+});
 
 // for each lb:
 //     exec("killall node")
