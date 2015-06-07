26d1de436d83a0efa652ce6c2cc4f4a73cf0c76f
Thalley <mrthalley@gmail>
2015-06-06 23:47:56 -0700

    Updated reset script

--- emulab/reset.js
+++ emulab/reset.js
@@ -3,10 +3,19 @@ var hosts = require('../configs/hosts.json');
 var hostagents = require('../configs/hostagent.json');
 var constant = require('../configs/constants.json');
 var lbs = require('../configs/slaves.json');
+
 var expname = (exec("hostname").toString().split(/[.]/))[1];
+var experiments = exec("ls /proj/Reactor/exp").toString().split(/[\n]/);
+for (var i = 0; i < experiments.length; i++)
+	if (experiments[i].toLowerCase() == expname){
+		expname = "/proj/Reactor/exp/" + experiments[i] + "/tbdata/tbreport.log";
+		break;
+	}
+}
+
 hosts.hosts.forEach(function(host){
     var ssh = "ssh thalley@" + host.IP + " ";
-    exec(ssh + "killall node");
+    exec(ssh + "'killall node'");
     exec(ssh + "/proj/Reactor/software_load_balancer/emulab/" + expname + "/clientstartupscript.sh");
 });
 
